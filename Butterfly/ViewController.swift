98e23a634527b41519a9592743e5a75633f087bd
Summer Hasama <summerhasama@gmail.com>
2020-12-28 14:34:52 -0800

    Butterfly app

--- Butterfly/ViewController.swift
+++ Butterfly/ViewController.swift
@@ -24,22 +24,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var MP_label: UILabel!
     @IBOutlet weak var DE_label: UILabel!
     
+    var quote = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        
+        // Get the quote of the day from the API
+        let url = URL(string:"https://quotes.rest/qod.json")!
+        let task = URLSession.shared.dataTask(with: url) { [self] (data, response, error) in
+            if let error = error {
+               print(error.localizedDescription)
+            }else if let data = data{
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let dataArray = ((dataDictionary["contents"]as! [String:Any])["quotes"]) as! [[String:Any]]
+                quote = dataArray.first!["quote"] as! String
+                quoteSet()
+            }
+        }
+        task.resume()
+      
+        // Butterflies header label
         butterflyLabel.frame = CGRect(x: 0, y: 0, width: 645, height: 61)
         butterflyLabel.backgroundColor = .white
         butterflyLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         butterflyLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 40)
         
+        // Quote label
         quoteLabel.frame = CGRect(x: 0, y: 0, width: 246, height: 33)
         quoteLabel.backgroundColor = .white
         quoteLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
         quoteLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 18)
 
         
+        // round the corners of the images
         image1.layer.cornerRadius = 15
         image2.layer.cornerRadius = 15
         image3.layer.cornerRadius = 15
@@ -65,10 +84,16 @@ class ViewController: UIViewController {
         DE_label.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
         DE_label.textAlignment = .center
         
-        
-        // Do any additional setup after loading the view.
+
     }
-
-
+    
+    // set the quote label after call to API
+    func quoteSet(){
+        DispatchQueue.main.async {
+            self.quoteLabel.text = self.quote
+        }
+    }
+    
+    
 }
-
+        
